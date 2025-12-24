7ab66c24f76d4ca315babfe1ef7026d3dc1493d7
theg1239 <thegreatestdropper@gmail.com>
2025-12-24 05:04:56 +0400

    refactor: implement dynamic fetching of Open Graph images and enhance responsive layout for blog components

--- src/data/blogData.ts
+++ src/data/blogData.ts
@@ -1,70 +1,53 @@
 export const blogData = [
   {
-    bgurl: "/blogs/chfailure-blog.png",
     link: "https://blog.acmvit.in/the-cryptic-hunt-2024-blog",
   },
   {
-    bgurl: "/blogs/computervision-blog.png",
     link: "https://blog.acmvit.in/how-do-computers-see-us",
   },
   {
-    bgurl: "/blogs/examcooker-blog.png",
     link: "https://blog.acmvit.in/the-chefs-secrets-the-story-behind-examcooker",
   },
   {
-    bgurl: "/blogs/githappens-blog.png",
     link: "https://blog.acmvit.in/git-happens",
   },
   {
-    bgurl: "/blogs/computervision-blog.png",
     link: "https://blog.acmvit.in/self-improving-ai",
   },
   {
-    bgurl: "/blogs/defi-blog.png",
     link: "https://blog.acmvit.in/defi-101",
   },
   {
-    bgurl: "/blogs/microchips-blog.png",
     link: "https://blog.acmvit.in/the-metaverses-cousin-you-didnt-know-existed-meet-the-digital-twin",
   },
   {
-    bgurl: "/blogs/quantum-blog.png",
     link: "https://blog.acmvit.in/quantum-timebomb",
   },
   {
-    bgurl: "/blogs/redis-blog.png",
     link: "https://blog.acmvit.in/redis-a-stellar-intro",
   },
   {
-    bgurl: "/blogs/rc-blog.png",
     link: "https://blog.acmvit.in/rc",
   },
   {
-    bgurl: "/blogs/microchips-blog.png",
     link: "https://blog.acmvit.in/how-i-met-your-ai-the-matrix-of-microchips",
   },
   {
-    bgurl: "/blogs/microinteraction-blog.png",
     link: "https://blog.acmvit.in/micro-interactions",
   },
   {
-    bgurl: "/blogs/quantum-blog.png",
     link: "https://blog.acmvit.in/beyond-the-totem-ai-driven-dream-manipulation-the-future-of-lucid-dreaming",
   },
   {
-    bgurl: "/blogs/microchips-blog.png",
     link: "https://blog.acmvit.in/honey-i-shrunk-the-ai-quantizing-llms-for-edge-hardware",
   },
   {
-    bgurl: "/blogs/microinteraction-blog.png",
     link: "https://blog.acmvit.in/color-psychology-in-web-design",
   },
   {
-    bgurl: "/blogs/githappens-blog.png",
     link: "https://blog.acmvit.in/cybersecurity-fueling-chaos",
   },
   {
-    bgurl: "/blogs/computervision-blog.png",
     link: "https://blog.acmvit.in/the-llm-world",
   },
 ];
